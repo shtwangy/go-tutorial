4ac34dc4ff3e57b8bc351a633969ccbd2c33495d
shtwangy <shtwangy@gmail.com>
2020-11-06 19:35:10 +0900

    feat(echo-api.go): create hello function.

--- echo-api.go
+++ echo-api.go
@@ -7,8 +7,10 @@ import (
 
 func main() {
     e := echo.New()
-    e.GET("/", func(c echo.Context) error {
-        return c.String(http.StatusOK, "Hello, Echo.")
-    })
+    e.GET("/", hello)
     e.Logger.Fatal(e.Start(":8080"))
+}
+
+func hello(c echo.Context) error {
+    return c.String(http.StatusOK, "Hello, Echo.")
 }
