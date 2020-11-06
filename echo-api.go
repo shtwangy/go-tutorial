031154520d36267070b52f192e20fe13664bb544
shtwangy <shtwangy@gmail.com>
2020-11-06 19:36:43 +0900

    feat(echo-api.go): create hello id routing.

--- echo-api.go
+++ echo-api.go
@@ -8,9 +8,15 @@ import (
 func main() {
     e := echo.New()
     e.GET("/", hello)
+    e.GET("/:id", helloId)
     e.Logger.Fatal(e.Start(":8080"))
 }
 
 func hello(c echo.Context) error {
     return c.String(http.StatusOK, "Hello, Echo.")
+}
+
+func helloId(c echo.Context) error {
+    id := c.Param("id")
+    return c.String(http.StatusOK, "Hello "+id)
 }
